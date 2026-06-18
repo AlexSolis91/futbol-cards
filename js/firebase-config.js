c63a902c6a49fde834933975db82ac0694441f2f
AlexSolis91 <solisalex8291@gmail.com>
2026-06-18 17:05:31 -0600

    Add js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,13 @@
+// ============================================================
+// CONFIGURACIÓN DE FIREBASE — futbol-cards
+// Proyecto: futbol-cards-3b3c8
+// ============================================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCN3ZW6besH6Hn9M-qHvMwc5xBdiEh6yu4",
+  authDomain: "futbol-cards-3b3c8.firebaseapp.com",
+  projectId: "futbol-cards-3b3c8",
+  storageBucket: "futbol-cards-3b3c8.firebasestorage.app",
+  messagingSenderId: "1043294224503",
+  appId: "1:1043294224503:web:50ecf61ec6c7dbbb1c8672",
+};
